0bdb7ef771c97504bd6b8e61dd5377152f7e1899
hon_ya <spaceodyssey2008@gmail.com>
2016-07-17 16:13:41 +0900

    (D3D12HelloBundles) Bundle を使うサンプルを作成
    * HelloTriangle をベースとする

---
+++ D3D12HelloBundles/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SharpDX.Windows;
+
+namespace D3D12HelloBundles
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var form = new RenderForm("D3D12 Hello Bundles")
+            {
+                Width = 1280,
+                Height = 720,
+            };
+            form.Show();
+
+            using (var app = new HelloBundles())
+            {
+                app.Initialize(form);
+
+                using (var loop = new RenderLoop(form))
+                {
+                    while (loop.NextFrame())
+                    {
+                        app.Update();
+                        app.Render();
+                    }
+                }
+            }
+        }
+    }
+}
